2c0d3fc706501d0f55a9be20483f18a11ab6976a
Natan Felles <natanfelles@gmail.com>
2021-06-16 15:53:02 -0300

    Disable PHPMD tests

--- ci-test.sh
+++ ci-test.sh
@@ -5,7 +5,7 @@ commands=(
 	"composer install"
 	"composer normalize --dry-run --indent-size=1 --indent-style=tab"
 	"vendor/bin/php-cs-fixer fix --diff --dry-run --verbose"
-	"vendor/bin/phpmd src xml phpmd.xml"
+	#"vendor/bin/phpmd src xml phpmd.xml"
 	"vendor/bin/phpstan analyse -vvv"
 	"vendor/bin/phpunit"
 	"phpdoc"
