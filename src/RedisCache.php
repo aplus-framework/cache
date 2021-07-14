b931edd2e8b349d32cb47bade62bf4d23df51b10
Natan Felles <natanfelles@gmail.com>
2021-07-14 11:04:09 -0300

    Update default header comments

--- src/RedisCache.php
+++ src/RedisCache.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 /*
- * This file is part of The Framework Cache Library.
+ * This file is part of Aplus Framework Cache Library.
  *
  * (c) Natan Felles <natanfelles@gmail.com>
  *
