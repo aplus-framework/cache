80da58a5ee415a5e6d9d7fac83c7f9b5faf9f702
Natan Felles <natanfelles@gmail.com>
2022-05-19 19:52:26 -0300

    Update MemcachedCache tests

--- tests/MemcachedCacheMultiServerTest.php
+++ tests/MemcachedCacheMultiServerTest.php
@@ -40,4 +40,48 @@ final class MemcachedCacheMultiServerTest extends MemcachedCacheTest
             ],
         ]);
     }
+
+    public function testHostInPool() : void
+    {
+        $configs = [
+            'servers' => [
+                [
+                    'host' => \getenv('MEMCACHED_HOST'),
+                ],
+                [
+                    'host' => \getenv('MEMCACHED_HOST'),
+                ],
+            ],
+        ];
+        new MemcachedCache(
+            $configs,
+            $this->prefix,
+            $this->serializer,
+            $this->getLogger()
+        );
+        self::assertSame(
+            'Cache (memcached): Server pool already has '
+            . \getenv('MEMCACHED_HOST') . ':11211',
+            $this->getLogger()->getLastLog()->message
+        );
+    }
+
+    public function testCouldNotConnect() : void
+    {
+        $configs = [
+            'servers' => [
+                [
+                    'host' => '192.168.0.1',
+                ],
+            ],
+        ];
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Cache (memcached): Could not connect to any server');
+        new MemcachedCache(
+            $configs,
+            $this->prefix,
+            $this->serializer,
+            $this->getLogger()
+        );
+    }
 }
